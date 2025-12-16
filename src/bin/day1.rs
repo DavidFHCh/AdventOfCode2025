87f59b04f4036208f31bac4b5e21d64883eab971
David Felipe Hernandez Chiapa <davifep.96@gmail.com>
2025-12-15 23:25:28 -0600

    Refactor Day 1 solution: streamline input handling and improve zero counting logic

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -2,26 +2,50 @@ use advent_of_code2025::fetch_input;
 
 #[tokio::main]
 async fn main() {
-    println!("Fetching Day 1 input...");
     let input = fetch_input(1).await;
+    part_one(&input).await;
+    println!("---");
+    part_two(&input).await;
+}
 
+async fn part_one(input: &str) {
     let mut current = 50;
     let mut counter_over_zeroes = 0;
-    
+
     for line in input.lines() {
         let direction = line.chars().next().unwrap();
         let mut value: i32 = line[1..].parse().unwrap();
         if direction == 'L' {
             value *= -1;
-            value += 100;
         }
-        //println!("Moving {} by {}", direction, value);
         current += value;
-        current = current % 100;
+        current = current.rem_euclid(100);
         if current == 0 {
             counter_over_zeroes += 1;
         }
     }
     println!("Final position: {}", current);
     println!("Number of times over 0: {}", counter_over_zeroes);
+}
+
+async fn part_two(input: &str) {
+    let mut current: i32 = 50;
+    let mut visited_zeroes = 0;
+
+    for line in input.lines() {
+        let direction = line.chars().next().unwrap();
+        let mut value: i32 = line[1..].parse().unwrap();
+        if direction == 'L' {
+            value *= -1;
+        }
+
+        let previous = current;
+        current += value;
+
+        visited_zeroes += (current.div_euclid(100) - previous.div_euclid(100)).abs();
+        
+        current = current.rem_euclid(100);
+    }
+    println!("Final position: {}", current);
+    println!("Number of visited 0s: {}", visited_zeroes);
 }
