8e9fae94339efceee580f381b52e1f7d1d643f52
David Felipe Hernandez Chiapa <davifep.96@gmail.com>
2025-12-16 00:40:57 -0600

    Refactor part_one and part_two functions: add return types and enhance zero crossing logic

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -8,7 +8,7 @@ async fn main() {
     part_two(&input).await;
 }
 
-async fn part_one(input: &str) {
+async fn part_one(input: &str) -> i32 {
     let mut current = 50;
     let mut counter_over_zeroes = 0;
 
@@ -26,9 +26,10 @@ async fn part_one(input: &str) {
     }
     println!("Final position: {}", current);
     println!("Number of times over 0: {}", counter_over_zeroes);
+    counter_over_zeroes
 }
 
-async fn part_two(input: &str) {
+async fn part_two(input: &str) -> i32 {
     let mut current: i32 = 50;
     let mut visited_zeroes = 0;
 
@@ -39,13 +40,78 @@ async fn part_two(input: &str) {
             value *= -1;
         }
 
+        print!("{}{} -> ", direction, value.abs());
         let previous = current;
+        print!("{} to ", current);
         current += value;
+        print!("{} ", current);
 
-        visited_zeroes += (current.div_euclid(100) - previous.div_euclid(100)).abs();
-        
+        let mut crossings = (current.div_euclid(100) - previous.div_euclid(100)).abs();
+        if direction == 'L' {
+            if previous == 0 {
+                crossings -= 1
+            }
+            if current.rem_euclid(100) == 0 {
+                crossings += 1;
+            }
+        }
+        print!("Crossings: {} \n", crossings);
+        visited_zeroes += crossings;
+
         current = current.rem_euclid(100);
     }
     println!("Final position: {}", current);
     println!("Number of visited 0s: {}", visited_zeroes);
+    visited_zeroes
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_l150_should_be_2() {
+        let result = part_two("L150").await;
+        assert_eq!(result, 2);
+    }
+
+    #[tokio::test]
+    async fn test_r150_should_be_2() {
+        let result = part_two("R150").await;
+        assert_eq!(result, 2);
+    }
+
+    #[tokio::test]
+    async fn test_r_start_at_0() {
+        let result = part_two("L50\nR100").await;
+        assert_eq!(result, 2);
+    }
+
+    #[tokio::test]
+    async fn test_l_start_at_0() {
+        let result = part_two("L50\nL100").await;
+        assert_eq!(result, 2);
+    }
+
+    #[tokio::test]
+    async fn test_100() {
+        let result = part_two("L100\nR50").await;
+        assert_eq!(result, 2);
+    }
+
+    #[tokio::test]
+    async fn test_example_part_two() {
+        let input = "L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82";
+        let result = part_two(input).await;
+        assert_eq!(result, 6);
+    }
 }
